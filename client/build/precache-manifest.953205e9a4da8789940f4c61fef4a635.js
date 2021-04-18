137367d832cb06ff3edd71bbd1ba6c10dd87578e
Brown Mamba <brdyherrera@gmail.com>
2021-04-17 20:05:19 -0400

    Adding production react files for heroku

---
+++ client/build/precache-manifest.953205e9a4da8789940f4c61fef4a635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbac5e0f7272c96d5738172d3c09484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3720cea001e232b25e88",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "576d60b80a5f97e90828",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "3720cea001e232b25e88",
+    "url": "/static/js/2.0d8fc218.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0d8fc218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576d60b80a5f97e90828",
+    "url": "/static/js/main.960244b0.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
